ca93a754bde764867d7ffe34aa58ac3e11faf7b6
Karim Radhouani <medkarimrdi@gmail.com>
2022-05-18 14:04:11 -0700

    go mod tidy and k8s impl placeholder

---
+++ registrator/k8s.go
@@ -0,0 +1,49 @@
+package registrator
+
+import (
+	"context"
+
+	"github.com/yndd/ndd-runtime/pkg/logging"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func Newk8sRegistrator(opts ...Option) (Registrator, error) {
+	return &k8sRegistrator{}, nil
+}
+
+// consul implements the Registrator interface
+type k8sRegistrator struct {
+	client client.Client
+	log    logging.Logger
+}
+
+func (r *k8sRegistrator) WithLogger(log logging.Logger) {
+	r.log = log
+}
+
+func (r *k8sRegistrator) WithClient(c client.Client) {
+	r.client = c
+}
+
+func (r *k8sRegistrator) Init(ctx context.Context) {}
+
+func (r *k8sRegistrator) Register(ctx context.Context, s *Service) {
+
+}
+
+func (r *k8sRegistrator) DeRegister(ctx context.Context, id string) {}
+
+func (r *k8sRegistrator) Query(ctx context.Context, serviceName string, tags []string) ([]*Service, error) {
+	return nil, nil
+}
+
+func (r *k8sRegistrator) GetEndpointAddress(ctx context.Context, serviceName string, tags []string) (string, error)
+
+func (r *k8sRegistrator) Watch(ctx context.Context, serviceName string, tags []string) chan *ServiceResponse {
+	return nil
+}
+
+func (r *k8sRegistrator) WatchCh(ctx context.Context, serviceName string, tags []string, ch chan *ServiceResponse) {
+}
+
+func (r *k8sRegistrator) StopWatch(serviceName string) {}
